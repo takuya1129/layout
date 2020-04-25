75aec768cbdfac6e4aa6d10d27df9292b970ff01
Takuya Kanazawa <takuya1991takuya1129@gmail.com>
2020-04-25 13:15:14 +0900

    Initial Commit

---
+++ TabBarContoller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarContoller
+//
+//  Created by 金澤拓弥 on 2020/04/25.
+//  Copyright © 2020 Dotinstall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabBarContollerTests/TabBarContollerTests.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarContollerTests.swift
+//  TabBarContollerTests
+//
+//  Created by 金澤拓弥 on 2020/04/25.
+//  Copyright © 2020 Dotinstall. All rights reserved.
+//
+
+import XCTest
+@testable import TabBarContoller
+
+class TabBarContollerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
